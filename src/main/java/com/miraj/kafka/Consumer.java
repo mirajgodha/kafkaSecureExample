9802af2d0ac26bfa592b1a37e84c2dd8d0114bf7
Miraj Godha <mirajgodha@Miraj-13-inch-Mac.local>
2019-11-14 17:03:36 +0530

    changed code

--- src/main/java/com/miraj/kafka/Consumer.java
+++ src/main/java/com/miraj/kafka/Consumer.java
@@ -22,8 +22,8 @@ public class Consumer {
         props.put("security.protocol", "SASL_PLAINTEXT");
         props.put("sasl.kerberos.service.name", "Kafka");
         props.put("sasl.mechanism", "GSSAPI");
-        String keytabLocation = "/etc/security/keytabs/cdap.headless.keytab";
-        String principal = "cdap-raf010-reflex-platform@GVS.GGN";
+        String keytabLocation = "/etc/security/keytabs/kafka.service.keytab";
+        String principal = "kafka/raf010-slv-04.cloud.in.guavus.com@GVS.GGN";
         
         props.put(SASL_JAAS_CONFIG, String.format("com.sun.security.auth.module.Krb5LoginModule required \n" +
                 "        useKeyTab=true \n" +
@@ -32,31 +32,33 @@ public class Consumer {
                 "        renewTicket=true  \n" +
                 "        keyTab=\"%s\" \n" +
                 "        principal=\"%s\";",
-        keytabLocation, principal));
-        KafkaConsumer<String, String> consumer=null;
-        // Checks connection by extracting topics.
-        try  {
-        	consumer = new KafkaConsumer<>(props);
+				keytabLocation, principal));
+		KafkaConsumer<String, String> consumer = null;
+		// Checks connection by extracting topics.
+		try {
+			consumer = new KafkaConsumer<>(props);
 //        	System.out.println("Number of topis are: " + consumer.listTopics().size());
 //        	consumer.listTopics().forEach((k,v) -> System.out.println("keys" + k + "Values"+ v));
-        	
-        	
+
 //        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(props);
-        List<String> topics = new ArrayList<String>();
-        topics.add("test_topic");
-        consumer.subscribe(topics);
-            while (true){
-                ConsumerRecords<String, String> records = consumer.poll(10);
-                for (ConsumerRecord<String, String> record: records){
-                    System.out.println(String.format("Topic - %s, Partition - %d, Value: %s", record.topic(), record.partition(), record.value()));
-                }
-            }
-        	
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }finally {
-        	consumer.close();
-        }
+			List<String> topics = new ArrayList<String>();
+			topics.add("test_topic");
+			consumer.subscribe(topics);
+			int i = 0;
+			while (i++ < 10) {
+				System.out.println("going to print : recores");
+				ConsumerRecords<String, String> records = consumer.poll(10);
+				for (ConsumerRecord<String, String> record : records) {
+					System.out.println(String.format("Topic - %s, Partition - %d, Value: %s", record.topic(),
+							record.partition(), record.value()));
+				}
+			}
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} finally {
+			consumer.close();
+		}
     }
 }
