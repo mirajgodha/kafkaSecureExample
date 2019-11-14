1e8e13c09e37b0c02a8997e3629f16901ade76aa
Miraj Godha <mirajgodha@Miraj-13-inch-Mac.local>
2019-11-14 18:01:14 +0530

    removed strings

--- src/main/java/com/miraj/kafka/Consumer.java
+++ src/main/java/com/miraj/kafka/Consumer.java
@@ -1,10 +1,11 @@
 package com.miraj.kafka;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-
+import org.apache.kafka.common.config.SaslConfigs;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,22 +13,24 @@ import java.util.Properties;
 
 public class Consumer {
 
-	public static final String SASL_JAAS_CONFIG = "sasl.jaas.config";
 	
     public static void main(String[] args) {
-        Properties props = new Properties();
+    
+    	KafkaConsumer<String, String> consumer = null;
+    	String keytabLocation = "/etc/security/keytabs/cdap.headless.keytab";
+    	String principal = "cdap-raf010-reflex-platform@GVS.GGN";
+
+    	Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG , "raf010-slv-04.cloud.in.guavus.com:6667");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("group.id", "test_topic");
-        props.put("security.protocol", "SASL_PLAINTEXT");
-        props.put("sasl.kerberos.service.name", "kafka");
-        props.put("sasl.mechanism", "GSSAPI");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test_topic");
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
+        props.put(SaslConfigs.SASL_KERBEROS_SERVICE_NAME, "kafka");
+        props.put(SaslConfigs.SASL_MECHANISM, "GSSAPI");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        String keytabLocation = "/etc/security/keytabs/cdap.headless.keytab";
-        String principal = "cdap-raf010-reflex-platform@GVS.GGN";
         
-        props.put(SASL_JAAS_CONFIG, String.format("com.sun.security.auth.module.Krb5LoginModule required \n" +
+        props.put(SaslConfigs.SASL_JAAS_CONFIG, String.format("com.sun.security.auth.module.Krb5LoginModule required \n" +
                 "        useKeyTab=true \n" +
                 "        storeKey=true  \n" +
                 "        useTicketCache=false  \n" +
@@ -35,12 +38,12 @@ public class Consumer {
                 "        keyTab=\"%s\" \n" +
                 "        principal=\"%s\";",
 				keytabLocation, principal));
-		KafkaConsumer<String, String> consumer = null;
+        
 		// Checks connection by extracting topics.
 		try {
 			consumer = new KafkaConsumer<>(props);
-//        	System.out.println("Number of topis are: " + consumer.listTopics().size());
-//        	consumer.listTopics().forEach((k,v) -> System.out.println("keys" + k + "Values"+ v));
+        	System.out.println("Number of topis are: " + consumer.listTopics().size());
+        	consumer.listTopics().forEach((k,v) -> System.out.println("keys" + k + "Values"+ v));
 
 //        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(props);
 			List<String> topics = new ArrayList<String>();
