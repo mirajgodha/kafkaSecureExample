b068cdaa6670515b88263c9c9890269186a1c2c5
Miraj Godha <mirajgodha@Miraj-13-inch-Mac.local>
2019-11-14 14:49:14 +0530

    keytabs

--- src/main/java/com/miraj/kafka/Consumer.java
+++ src/main/java/com/miraj/kafka/Consumer.java
@@ -4,12 +4,16 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
+import src.main.java.co.cask.hydrator.plugin.common.String;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 public class Consumer {
 
+	public static final String SASL_JAAS_CONFIG = "sasl.jaas.config";
+	
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put("bootstrap.servers", "raf010-slv-04.cloud.in.guavus.com:9092,raf010-slv-05.cloud.in.guavus.com:9092");
@@ -18,6 +22,17 @@ public class Consumer {
         props.put("group.id", "test-group");
         props.put("security.protocol", "SASL_PLAINTEXT");
         props.put("sasl.kerberos.service.name", "kafka");
+        String keytabLocation = "/etc/security/keytabs/cdap.headless.keytab";
+        String principal = "cdap-raf010-reflex-platform@GVS.GGN";
+        
+        props.put(SASL_JAAS_CONFIG, String.format("com.sun.security.auth.module.Krb5LoginModule required \n" +
+                "        useKeyTab=true \n" +
+                "        storeKey=true  \n" +
+                "        useTicketCache=false  \n" +
+                "        renewTicket=true  \n" +
+                "        keyTab=\"%s\" \n" +
+                "        principal=\"%s\";",
+        keytabLocation, principal));
 
         // Checks connection by extracting topics.
         try  {
